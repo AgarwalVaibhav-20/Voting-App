08efe1ea20eeba292123f71babfcd542144d4f5d
its-shivam1008 <tanil5960@gmail.com>
2024-10-16 01:53:11 +0530

    moving deleteImageFromCloudinary from client side to server side as crypto cannot be used with client side.

--- src/upload/uploadImageToCloudinary.js
+++ src/upload/uploadImageToCloudinary.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import crypto from 'crypto';
-// import crypto
-
 export const uploadImageToCloudinary = async (file) => {
   
   const formData = new FormData();
@@ -23,35 +20,3 @@ export const uploadImageToCloudinary = async (file) => {
   }
 };
 
-export const deleteImageFromCloudinary = async (imgUrl) => {
-  try {
-    
-    const publicId = imgUrl.split('/').pop().split('.')[0];
-    const data =  {
-      public_id: publicId,
-      api_key: import.meta.env.VITE_CLOUDINARY_API_KEY,
-      timestamp: Math.floor(Date.now() / 1000),
-      signature: generateSignature(publicId, import.meta.env.VITE_CLOUDINARY_API_SECRET) 
-    }
-
-    const response = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/destroy`, 
-      data
-    );
-
-    console.log('Delete response:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Error deleting image from Cloudinary:', error);
-    throw error;
-  }
-}
-
-
-const generateSignature = (publicId, apiSecret) => {
-  // const crypto = require('crypto');
-  const timestamp = Math.floor(Date.now() / 1000);
-  const signature = crypto.createHash('sha1')
-    .update(`public_id=${publicId}&timestamp=${timestamp}${apiSecret}`)
-    .digest('hex');
-  return signature;
-};

bd5ebbbd4982cbadb2bdb5952ed28c55a383f840
its-shivam1008 <tanil5960@gmail.com>
2024-10-19 15:50:12 +0530

    add favicon and logo

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { MdMenu, MdClose } from "react-icons/md";
-import { CiLogout,CiLogin, CiUser } from "react-icons/ci";
+import { CiLogout, CiLogin, CiUser } from "react-icons/ci";
 import { TfiHelpAlt } from "react-icons/tfi";
 import { RiAdminLine } from "react-icons/ri";
 import profileImage from "../assets/profileImage.jpg";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthState";
+import logopng from "../assets/logoPng.png";
 
 
 export default function Myhead() {
@@ -16,10 +17,10 @@ export default function Myhead() {
 
   const navigate = useNavigate();
 
-  const { loggedUser, isLoggedIn, logout, fetchUser , status} = useAuth();
+  const { loggedUser, isLoggedIn, logout, fetchUser, status } = useAuth();
 
 
-  
+
 
   const handleClickOnLogout = () => {
     closeProfileDropdown();
@@ -46,8 +47,11 @@ export default function Myhead() {
         style={{ position: "sticky", top: 0, zIndex: 1000 }}
         className="bg-white flex justify-between items-center px-6 py-3 shadow z-50 max-sm:relative"
       >
-        <h1 className="text-2xl">VoteGov</h1>
-        {}
+        <img
+          src={logopng} alt="logo" 
+          className="w-20"
+        />
+        { }
         <div>
           <div
             className="lg:hidden text-2xl cursor-pointer"
@@ -56,21 +60,18 @@ export default function Myhead() {
             {isOpen ? <MdClose /> : <MdMenu />}
           </div>
           <nav
-            className={`lg:flex items-center justify-center lg:space-x-8 ${
-              isOpen ? "block" : "hidden"
-            } lg:block`}
+            className={`lg:flex items-center justify-center lg:space-x-8 ${isOpen ? "block" : "hidden"
+              } lg:block`}
           >
             <ul
-              className={`flex text-lg max-lg:bg-white items-center justify-center max-lg:items-start ${
-                isOpen ? "block" : "hidden"
-              } lg:flex  lg:space-x-8 max-lg:absolute max-lg:top-[45.9px] max-lg:right-[-1px] max-lg:border max-lg:w-full max-lg:flex max-lg:flex-col max-lg:translate-y-[2px]`}
+              className={`flex text-lg max-lg:bg-white items-center justify-center max-lg:items-start ${isOpen ? "block" : "hidden"
+                } lg:flex  lg:space-x-8 max-lg:absolute max-lg:top-[45.9px] max-lg:right-[-1px] max-lg:border max-lg:w-full max-lg:flex max-lg:flex-col max-lg:translate-y-[2px]`}
             >
               <li className="max-lg:w-[100%] ">
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"
                     } border-b bg-white  lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
@@ -81,8 +82,7 @@ export default function Myhead() {
                 <NavLink
                   to="/voting"
                   className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"
                     } border-b bg-white  lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
@@ -93,8 +93,7 @@ export default function Myhead() {
                 <NavLink
                   to="/dashboard"
                   className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-200 ${
-                      isActive ? "text-orange-700" : "text-gray-700"
+                    `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"
                     } border-b bg-white  lg:border-0 hover:text-orange-700 lg:p-0`
                   }
                 >
@@ -105,19 +104,19 @@ export default function Myhead() {
               {/* Profile Dropdown */}
               <li className="relative max-lg:w-[100%]">
                 {
-                  isLoggedIn ? 
-                  <button
-                    onClick={toggleProfileDropdown}
-                    className="flex items-center space-x-2 py-2 pr-4 pl-3 text-gray-700 bg-white  lg:border-0 hover:text-orange-700 lg:p-0"
-                  >
-                    {/* Profile image */}
-                    <img
-                      src={!status && loggedUser?.profilePic ? loggedUser.profilePic : 'https://res.cloudinary.com/dljgidqrq/image/upload/v1728844361/ljmzm14uujnfl1nqwlxi.jpg'}
-                      alt="Profile"
-                      className="w-8 h-8 rounded-full object-cover"
-                    />
-                    <span className='font-bold'>{loggedUser?.name.split(' ')[0]}</span>
-                  </button> : <NavLink to="/login" className="block px-4 py-2 font-bold text-orange-400 hover:text-orange-600" onClick={closeProfileDropdown}>Login / Signup</NavLink> 
+                  isLoggedIn ?
+                    <button
+                      onClick={toggleProfileDropdown}
+                      className="flex items-center space-x-2 py-2 pr-4 pl-3 text-gray-700 bg-white  lg:border-0 hover:text-orange-700 lg:p-0"
+                    >
+                      {/* Profile image */}
+                      <img
+                        src={!status && loggedUser?.profilePic ? loggedUser.profilePic : 'https://res.cloudinary.com/dljgidqrq/image/upload/v1728844361/ljmzm14uujnfl1nqwlxi.jpg'}
+                        alt="Profile"
+                        className="w-8 h-8 rounded-full object-cover"
+                      />
+                      <span className='font-bold'>{loggedUser?.name.split(' ')[0]}</span>
+                    </button> : <NavLink to="/login" className="block px-4 py-2 font-bold text-orange-400 hover:text-orange-600" onClick={closeProfileDropdown}>Login / Signup</NavLink>
                 }
 
                 {/* Dropdown menu */}
@@ -129,7 +128,7 @@ export default function Myhead() {
                     <li className="flex items-center space-x-2 p-4">
                       {/* Display the profile image inside the dropdown */}
                       <img
-                        src={!status && loggedUser?.profilePic  ? loggedUser.profilePic : 'https://res.cloudinary.com/dljgidqrq/image/upload/v1728844361/ljmzm14uujnfl1nqwlxi.jpg'}
+                        src={!status && loggedUser?.profilePic ? loggedUser.profilePic : 'https://res.cloudinary.com/dljgidqrq/image/upload/v1728844361/ljmzm14uujnfl1nqwlxi.jpg'}
                         alt="Profile"
                         className="w-10 h-10 rounded-full object-cover"
                       />

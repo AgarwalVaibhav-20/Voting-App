277f3b6d0c331289ea0a99257a38dd73938f9531
its-shivam1008 <tanil5960@gmail.com>
2024-10-19 11:26:19 +0530

    add party images in voting and dashpoard componenet

--- src/Component/Dashboard/VoterTable.jsx
+++ src/Component/Dashboard/VoterTable.jsx
@@ -1,6 +1,32 @@
 import PropTypes from 'prop-types';
+import BJPLOGO from "../../assets/BJPLOGO.png";
+import INCLOGO from "../../assets//INCLOGO.png";
+import SPALOGO from "../../assets/SPALOGO.png";
+import AAPLOGO from "../../assets/AAPLOGO.png";
+import aitc from "../../assets/aitc.png";
+import dmk from "../../assets/dmk.png";
+import tdp from "../../assets/tdp.png";
+import jdu from '../../assets/jdu.png';
+import shsubt from '../../assets/shsubt.png';
+import ncpsp from '../../assets/ncpsp.png'
+import SHS from '../../assets/ssh.png'
 
 const VoterTable = ({ voters }) => {
+
+  const parties = {
+    'BJP': BJPLOGO,
+    'INC': INCLOGO,
+    'SPA': SPALOGO,
+    'AAP': AAPLOGO,
+    'AITC': aitc,
+    'DMK': dmk,
+    'TDP': tdp,
+    'JD(U)': jdu,
+    'SHSUBT': shsubt,
+    'NCPSP': ncpsp,
+    'SHS': SHS,
+  }
+
   return (
     <div className="bg-white shadow-lg rounded-lg p-4 overflow-auto">
       <h3 className="text-xl font-semibold mb-4">Voter Participation</h3>
@@ -17,7 +43,13 @@ const VoterTable = ({ voters }) => {
             <tr key={index} className="text-center">
               <td className="py-2">{voter?._id}</td>
               <td className="py-2">{voter?.name}</td>
-              <td className="py-2">{voter?.votedFor?.party}</td>
+              <td className="py-2 flex gap-1 items-center justify-center">{voter?.votedFor?.party}
+                <img
+                  src={parties[voter?.votedFor?.party]}
+                  alt={voter?.votedFor?.party}
+                  className="w-8 h-8 max-sm:w-12 max-sm:h-12 max-sm:mb-2 max-sm:mr-3 rounded-full object-cover shadow-lg min-[0px]:max-md:hidden"
+                />
+              </td>
             </tr>
           ))}
         </tbody>
--- src/Component/VotingUser.jsx
+++ src/Component/VotingUser.jsx
@@ -18,7 +18,7 @@ import { GiCircleClaws } from "react-icons/gi";
 import { ImSpinner9 } from "react-icons/im";
 import { CgSpinner } from "react-icons/cg";
 
-const VotingPage = () => { 
+const VotingPage = () => {
   const [selectedParty, setSelectedParty] = useState("");
   const [selectedPartyId, setSelectedPartyId] = useState(null);
   const [submitted, setSubmitted] = useState(false);
@@ -29,24 +29,24 @@ const VotingPage = () => {
 
   const [fetchingCandidates, setFetchingCandidates] = useState(false);
 
-  
-  const parties = [
-    { name: "BJP", candidate: "Narendra Modi", logo: BJPLOGO },
-    { name: "INC", candidate: "Rahul Gandhi", logo: INCLOGO },
-    { name: "SPA", candidate: "Akhilesh Yadav", logo: SPALOGO},
-    { name: "AAP", candidate: "Arvind Kejriwal", logo: AAPLOGO },
-    { name: "AITC", candidate: "Mamata Banerjee", logo: aitc },
-    { name: "DMK", candidate: "M K Stalin", logo: dmk },
-    { name: "TDP", candidate: "N. Chandrababu Naidu", logo: tdp },
-    {name:"JD(U)" ,candidate:"Nitish Kumar" , logo:jdu},
-    {name:"SHSUBT" , candidate:"Uddhav Thackeray" , logo:shsubt},
-    {name:"NCPSP" , candidate:"Sharadchandra Pawar " , logo:ncpsp},
-    {name:"SHS" , candidate:"Eknath Shinde" , logo:SHS}
-  ];
+
+  const parties = {
+    'BJP':BJPLOGO,
+    'INC':INCLOGO,
+    'SPA':SPALOGO,
+    'AAP':AAPLOGO,
+    'AITC':aitc,
+    'DMK':dmk,
+    'TDP':tdp,
+    'JD(U)':jdu,
+    'SHSUBT':shsubt,
+    'NCPSP':ncpsp,
+    'SHS':SHS,
+  }
 
   const [candidates, setCandidates] = useState([]);
 
-  
+
 
   const fetchCandidates = async () => {
     setFetchingCandidates(true);
@@ -79,7 +79,7 @@ const VotingPage = () => {
   useEffect(() => {
     fetchCandidates()
   }, [])
-  
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -90,12 +90,12 @@ const VotingPage = () => {
       const response = await fetch(`${import.meta.env.VITE_BACKEND_PUBLIC_URL}/vote/${selectedPartyId}`, {
         method: 'PUT',
         headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
         }
       })
       const res = await response.json()
-      if(!response.ok){
+      if (!response.ok) {
         toast.error(res.message, {
           position: "bottom-right",
           autoClose: 5000,
@@ -107,7 +107,7 @@ const VotingPage = () => {
           theme: "colored",
           transition: Bounce,
         });
-      }else{
+      } else {
         toast.success(res.message, {
           position: "bottom-right",
           autoClose: 5000,
@@ -134,7 +134,7 @@ const VotingPage = () => {
 
   return (
     <>
-    <ToastContainer
+      <ToastContainer
         position="bottom-right"
         autoClose={5000}
         hideProgressBar={false}
@@ -147,75 +147,78 @@ const VotingPage = () => {
         theme="light"
         transition:Bounce
       />
-    <div className="min-h-screen flex w-full items-center justify-center bg-gray-100 p-6">
-      {!submitted ? (
-        <div className="w-full max-w-screen-xl">
-          <h2 className="text-4xl font-bold text-center mb-10 max-sm:text-2xl">
-            Electronic Voting Machine
-          </h2>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-wrap w-full justify-center items-center gap-3">
-              {candidates.map((party, index) => (
-                <div
-                  key={index}
-                  className={` flex w-full justify-between items-center p-6 md:p-8 border-2 rounded-xl bg-white shadow-lg cursor-pointer transition-all hover:scale-105 transform ${
-                    selectedPartyId === party?._id
-                      ? "border-blue-500 ring-4 ring-blue-400 shadow-xl"
-                      : "border-gray-300"
-                  }`}
-                  onClick={() => setSelectedPartyId(party?._id)}
-                >
-                  <img
-                    src={party?.profilePic}
-                    alt={party?.party}
-                    className="w-24 h-24 mb-6 max-sm:w-12 max-sm:h-12 max-sm:mb-2 max-sm:mr-3 rounded-full object-cover shadow-lg"
-                  />
-                  <div className="flex justify-center items-center w-full max-sm:mr-auto max-sm:ml-auto">
-                  <div className=" flex justify-center items-center max-sm:flex-col gap-3 max-sm:gap-2 ">
-                      <span className="text-2xl max-sm:text-base font-bold">
-                        {party?.party}
-                      </span>
-                      <p className="text-lg text-center font-light max-sm:text-xs max-sm:font-semibold">
-                        Candidate: {party?.name}
-                      </p>
+      <div className="min-h-screen flex w-full items-center justify-center bg-gray-100 p-6">
+        {!submitted ? (
+          <div className="w-full max-w-screen-xl">
+            <h2 className="text-4xl font-bold text-center mb-10 max-sm:text-2xl">
+              Electronic Voting Machine
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-wrap w-full justify-center items-center gap-3">
+                {candidates.map((party, index) => (
+                  <div
+                    key={index}
+                    className={` flex w-full justify-between items-center p-6 md:p-8 border-2 rounded-xl bg-white shadow-lg cursor-pointer transition-all hover:scale-105 transform ${selectedPartyId === party?._id
+                        ? "border-blue-500 ring-4 ring-blue-400 shadow-xl"
+                        : "border-gray-300"
+                      }`}
+                    onClick={() => setSelectedPartyId(party?._id)}
+                  >
+                    <img
+                      src={party?.profilePic}
+                      alt={party?.party}
+                      className="w-24 h-24 mb-6 max-sm:w-12 max-sm:h-12 max-sm:mb-2 max-sm:mr-3 rounded-full object-cover shadow-lg"
+                    />
+                    <div className="flex justify-center items-center w-full max-sm:mr-auto max-sm:ml-auto">
+                      <div className=" flex flex-col justify-center items-center max-sm:flex-col gap-3 max-sm:gap-2 ">
+                        <span className="text-2xl max-sm:text-base font-bold flex items-center gap-2">
+                          {party?.party}
+                          <img
+                            src={parties[party?.party]}
+                            alt={party?.party}
+                            className="w-10 h-10 max-sm:w-12 max-sm:h-12 max-sm:mb-2 max-sm:mr-3 rounded-full object-cover shadow-lg"
+                          />
+                        </span>
+                        <p className="text-lg text-center font-light max-sm:text-xs max-sm:font-semibold">
+                          Candidate: {party?.name}
+                        </p>
+                      </div>
                     </div>
+                    <div>
+                      <div
+                        className={` inline-block w-[25px] h-[25px] max-sm:h-5 max-sm:w-5 bg-green-600 rounded-full ${selectedPartyId === party?._id
+                            ? "bg-green-800"
+                            : "bg-red-600"
+                          }`}
+                      ></div>
+                    </div>
+                    {selectedPartyId === party?._id && (
+                      <div className="absolute inset-0 bg-blue-100 opacity-5 rounded-xl"></div>
+                    )}
                   </div>
-                 <div>
-                 <div
-                    className={` inline-block w-[25px] h-[25px] max-sm:h-5 max-sm:w-5 bg-green-600 rounded-full ${
-                      selectedPartyId === party?._id
-                        ? "bg-green-800"
-                        : "bg-red-600"
-                    }`}
-                  ></div>
-                 </div>
-                  {selectedPartyId === party?._id && (
-                    <div className="absolute inset-0 bg-blue-100 opacity-5 rounded-xl"></div>
-                  )}
-                </div>
-              ))}
-            </div>
-            <button
-              type="submit"
-              className="disabled:cursor-not-allowed disabled:bg-green-400 bg-green-600 text-white py-3 px-6 mt-10 w-full sm:w-64 mx-auto block rounded-lg text-xl max-sm:text-base hover:bg-green-700 transition duration-300"
-              disabled={!selectedPartyId || isSubmitting}
-            >
-              {isSubmitting ?  <div className='flex items-center justify-center space-x-2'><CgSpinner className='animate-spin size-5' /><div>Voting...</div></div>  : "Confirm Vote"}
-            </button>
-          </form>
-        </div>
-      ) : (
-        <div className="bg-white p-8 shadow-md rounded-md max-w-md w-full text-center">
-          <h2 className="text-2xl font-bold mb-6 max-sm:text-xl">
-            Thank you for voting!
-          </h2>
-          <p className="text-lg max-sm:text-base">
-            You voted for:{" "}
-            <span className="font-semibold">{selectedParty}</span>
-          </p>
-        </div>
-      )}
-    </div>
+                ))}
+              </div>
+              <button
+                type="submit"
+                className="disabled:cursor-not-allowed disabled:bg-green-400 bg-green-600 text-white py-3 px-6 mt-10 w-full sm:w-64 mx-auto block rounded-lg text-xl max-sm:text-base hover:bg-green-700 transition duration-300"
+                disabled={!selectedPartyId || isSubmitting}
+              >
+                {isSubmitting ? <div className='flex items-center justify-center space-x-2'><CgSpinner className='animate-spin size-5' /><div>Voting...</div></div> : "Confirm Vote"}
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="bg-white p-8 shadow-md rounded-md max-w-md w-full text-center">
+            <h2 className="text-2xl font-bold mb-6 max-sm:text-xl">
+              Thank you for voting!
+            </h2>
+            <p className="text-lg max-sm:text-base">
+              You voted for:{" "}
+              <span className="font-semibold">{selectedParty}</span>
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };

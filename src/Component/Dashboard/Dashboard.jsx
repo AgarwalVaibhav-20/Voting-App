3bb748f39c3b65f096f338c40039996f616c6fe2
its-shivam1008 <tanil5960@gmail.com>
2024-10-19 10:10:53 +0530

    add dashboard spinner

--- src/Component/Dashboard/Dashboard.jsx
+++ src/Component/Dashboard/Dashboard.jsx
@@ -4,45 +4,18 @@ import StatsCard from './StatsCard';
 import VoteChart from './VoterChart';
 import VoterTable from './VoterTable';
 import { ImSpinner9 } from 'react-icons/im';
+import { MdHowToVote } from 'react-icons/md';
 
 const Dashboard = () => {
 
   // Simulated Data
-  const [voters, setVoters] = useState([
-    { id: 1, name: 'Umang Sinha', vote: 'BJP' },
-    { id: 2, name: 'Priyanshu Patel', vote: 'INC' },
-    { id: 3, name: 'Aniket Mishra', vote: 'BJP' },
-    { id: 4, name: 'Amit Kumar', vote: 'INC' },
-    { id: 5, name: 'Vishal Kumar', vote: 'INC' },
-    { id: 6, name: 'Nayan Chaturvedi', vote: 'BJP' },
-    { id: 7, name: 'Arvind yadav', vote: 'AITC' },
-    { id: 8, name: 'Mohan Banerjee', vote: 'AITC' },
-    { id: 9, name: 'M K Gandhi', vote: 'DMK' },
-    { id: 10, name: 'Nischay Gupta', vote: 'TDP' },
-    { id: 11, name: 'Nitish Agarwal', vote: 'JD(U)' },
-    { id: 12, name: 'Varun Dhawan', vote: 'SHSUBT' },
-    { id: 13, name: 'Sharad pawar', vote: 'NCPSP' },
-    { id: 14, name: 'Ravi Shinde', vote: 'SHS' },
-    { id: 15, name: 'Subash ghosh', vote: 'AITC' },
-  ]);
+  const [voters, setVoters] = useState([]);
 
 
   const [fetchingVoters, setFetchingVoters] = useState(false);
   const [fetchingCandidates, setFetchingCandidates] = useState(false);
   const [fetchingCount, setFetchingCount] = useState(false);
-  const [voteData, setVoteData] = useState({
-    BJP: 340,
-    INC: 99,
-    SP: 37,
-    AITC: 29,
-    DMK: 22,
-    TDP: 22,
-    JDU:12,
-    SHSUBT:9,
-    NCPSP:8,
-    SHS:7,
-    Other:64
-  });
+  const [voteData, setVoteData] = useState(null);
   const [candidates, setCandidates] = useState([]);
   const [totalUserVoted, setTotalUserVoted] = useState();
   // const [voters, setVoters] = useState([]);
@@ -161,19 +134,19 @@ const Dashboard = () => {
       <HeaderDash/>
       <div className="container mx-auto p-4">
         {/* Top Stats Section */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
+        {voters.length > 0 ? <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
           <StatsCard title="Total Votes" value={totalVotes} />
           <StatsCard title="Leading Candidate" value={leadingCandidate?.party} />
           <StatsCard title="Voter Turnout" value={`${voters.length} Voters`} />
-        </div>
+        </div> : <MdHowToVote className='flex justify-center items-center size-20 animate-pulse text-orange-500'/>}
 
         {/* Chart and Voter List Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
             <h3 className="text-xl font-semibold mb-4">Voting Distribution</h3>
-            <VoteChart data={voteData} />
+           {voteData ? <VoteChart data={voteData} /> : <MdHowToVote className='size-20 animate-pulse text-orange-500 flex justify-center items-center'/>}
           </div>
-          <VoterTable voters={voters} />
+          {voters.length > 0 ? <VoterTable voters={voters} /> : <MdHowToVote className='size-20 animate-pulse text-orange-500 flex justify-center items-center'/>}
         </div>
       </div>
     </>
